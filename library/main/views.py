d8d53b13ba78831de232ae09c332704f2c63db34
norrljus <178391145+norrljus@users.noreply.github.com>
2024-12-27 21:29:14 +0500

    Initial Commit

---
+++ library/main/views.py
@@ -0,0 +1,44 @@
+from rest_framework import viewsets, status
+from rest_framework.decorators import api_view
+from rest_framework.generics import RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Book, Order
+from .serializers import BookSerializer
+
+
+@api_view(['GET'])
+def books_list(request):
+    phones = Book.objects.all()
+    serializer = BookSerializer(phones, many=True)
+    return Response(serializer.data)
+
+
+class CreateBookView(APIView):
+    def post(self, request):
+        data = request.data
+        serializer = BookSerializer(data=data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response({"message": "Книга успешно создана"})
+
+
+class BookDetailsView(RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookUpdateView(UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookDeleteView(DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
